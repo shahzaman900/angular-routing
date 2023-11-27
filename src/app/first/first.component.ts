57a1d37ab60132c028112971618abf6031af2f64
sheezu900 <shahzamanlive2@gmail.com>
2023-11-27 11:25:48 +0500

    complete setup import components, create routes array

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-first',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './first.component.html',
+  styleUrl: './first.component.scss'
+})
+export class FirstComponent {
+
+}
---
+++ src/app/second/second.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-second',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './second.component.html',
+  styleUrl: './second.component.scss'
+})
+export class SecondComponent {
+
+}
